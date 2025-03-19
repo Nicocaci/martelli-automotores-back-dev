c193b008d6e3e27a07dabf61ec1773ed3411eb68
Nicocaci <nicko.caci@gmail.com>
2025-03-19 11:22:44 -0300

    correxion de variables

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser())
 app.use(cors({
-        origin:"http://localhost:5173",
+        origin:"https://martelli-automotes-back-production.up.railway.app/",
         credentials: true,
 }));
 app.use(session({
