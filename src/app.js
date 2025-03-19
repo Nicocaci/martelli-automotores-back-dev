5a8d5ee70b68f474aefed2d09e7139d458e94a82
Nicocaci <nicko.caci@gmail.com>
2025-03-19 11:29:09 -0300

    arreglo

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser())
 app.use(cors({
-        origin:"https://martelli-automotes-back-production.up.railway.app",
+        origin:"https://martelli-automotores-front-production.up.railway.app",
         credentials: true,
 }));
 app.use(session({
