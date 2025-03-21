22fcbd7cd3e0a4f559257fa1fddc4fe9566f61fd
Nicocaci <nicko.caci@gmail.com>
2025-03-21 17:04:47 -0300

    push

--- src/database.js
+++
@@ -1,5 +0,0 @@
-// import mongoose from "mongoose";
-
-// mongoose.connect("mongodb+srv://nicocaci:nicocaci@coderhouse.ihpiu.mongodb.net/martelliAutomotores?retryWrites=true&w=majority&appName=coderhouse")
-//     .then(() => console.log("Conectado con la base de datos correctamente"))
-//     .catch(()=> console.log("Error al conectar la base de datos"))
